96d62d606011286f803c4cd2d8d69c5ca5820014
dogeystamp <dogeystamp@disroot.org>
2024-01-02 21:03:31 -0500

    add wifi "support"

--- roles/networking/connection/templates/rmconn.sh
+++ roles/networking/connection/templates/rmconn.sh
@@ -3,6 +3,6 @@
 sleep 30
 
 /usr/bin/nmcli connection down {{ net_interface }}      > /dev/null 2>&1
-/usr/bin/nmcli connection down "Wired connection 1" > /dev/null 2>&1
-/usr/bin/nmcli connection down wired                > /dev/null 2>&1
-/usr/bin/nmcli connection up   wired                > /dev/null
+/usr/bin/nmcli connection down "Wired connection 1"     > /dev/null 2>&1
+/usr/bin/nmcli connection down {{ conn_name }}          > /dev/null 2>&1
+/usr/bin/nmcli connection up   {{ conn_name }}          > /dev/null
